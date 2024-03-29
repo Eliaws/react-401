3071d28f8b5707c8034c68757887e821c2834c89
elias ouabala <e-eouabala@etu.loc>
2024-03-29 12:26:26 +0100

    Ajout Premiers composants Header & Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from './Components/Header';
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        <Header/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+          </Routes>
+        </BrowserRouter>
+        <Footer/>
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+
+function Home() {
+    return (
+      <div className="txt-2xl txt-red-600 mt-2 mb-2 h-[500px]">
+        <h1>Home</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
